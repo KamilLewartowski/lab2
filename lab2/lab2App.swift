5de49424b339f1920870b0e4ad426d09415f7501
a <a>
2024-04-27 08:11:03 +0200

    Initial Commit

---
+++ lab2/lab2App.swift
@@ -0,0 +1,17 @@
+//
+//  lab2App.swift
+//  lab2
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct lab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
